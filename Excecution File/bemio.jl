0d6270f1050e78f22b2be69ab7660a4c7c18d38f
gitSnJul <jelope@sandia.gov>
2024-06-27 19:58:19 -0600

    Update bemio.jl

--- Excecution File/bemio.jl
+++ Excecution File/bemio.jl
@@ -1,31 +1,40 @@
-###################################################################################################
-include("C:/Users/jelope/Desktop/Git/WEC-Sim/source/functions/BEMIO/readWAMIT.jl")
+include("C:/Users/jelope/Desktop/Git/WEC-Sim/source/functions/BEMIO/readWAMITv2.jl")
 include("C:/Users/jelope/Desktop/Git/WEC-Sim/source/functions/BEMIO/radiationIRFv2.jl")
-#include("C:/Users/jelope/Desktop/Git/WEC-Sim/source/functions/BEMIO/radiationIRFSS.jl")
-#include("C:/Users/jelope/Desktop/Git/WEC-Sim/source/functions/BEMIO/excitationIRF.jl")
-#include("C:/Users/jelope/Desktop/Git/WEC-Sim/source/functions/BEMIO/writeBEMIOH5.jl")
-#include("C:/Users/jelope/Desktop/Git/WEC-Sim/source/functions/BEMIO/plotBEMIO.jl")
-###################################################################################################
+include("C:/Users/jelope/Desktop/Git/WEC-Sim/source/functions/BEMIO/radiationIRFSSv2.jl")
+include("C:/Users/jelope/Desktop/Git/WEC-Sim/source/functions/BEMIO/excitationIRF.jl")
+include("C:/Users/jelope/Desktop/Git/WEC-Sim/source/functions/BEMIO/normalizeBEM.jl")
 
-hydro = Dict{Any, Any}()
-option = 0  # Set option: 0 for WAMIT, 1 for OSWEC
+# User input
+option = 0
 
 if option == 0
     # WAMIT
-    hydro = readWAMIT(filepath = "C:/Users/jelope/Desktop/Git/WEC-Sim/examples/RM3/hydroData/rm3.out")
-    data = radiationIRF(hydro)
-    hydro = radiationIRFSS(nothing, nothing)
-    hydro = excitationIRF(hydro, 157, nothing, nothing, nothing, nothing)
-    writeBEMIOH5(hydro)
+    hydro = readWAMIT("C:/Users/jelope/Desktop/Git/WEC-Sim/examples/RM3/hydroData/rm3.out")
+    hydro = radiationIRF(hydro, 60)
+    hydro = radiationIRFSS(hydro)
+    hydro = excitationIRF(hydro, 157)
+
+    # Print field values for comparison
+    print_hydro_data(hydro)
+
+    #writeBEMIOH5(hydro)
 
     # Plot hydro data
-    #plotBEMIO(hydro)
+    # plotBEMIO(hydro)
 elseif option == 1
     # OSWEC
-    hydro = readWAMIT(filepath = "C:/Users/jelope/Desktop/Git/WEC-Sim/examples/OSWEC/hydroData/oswec.out")
-    hydro = radiationIRF(hydro, 30, nothing, nothing, nothing, nothing)
-    hydro = radiationIRFSS(hydro, nothing, nothing)
-    hydro = excitationIRF(hydro, 30, nothing, nothing, nothing, nothing)
+    hydro = readWAMIT("C:/Users/jelope/Desktop/Git/WEC-Sim/examples/OSWEC/hydroData/oswec.out")
+
+    #hydro = radiationIRF(hydro, 30.0f0, 1001, 1001, minimum(hydro.w), maximum(hydro.w))
+    #hydro = radiationIRFSS(hydro)
+    #hydro = excitationIRF(hydro, 30, nothing, nothing, nothing, nothing)
+    #writeBEMIOH5(hydro)
+
+    # Print field values for comparison
+    print_hydro_data(hydro)
+
+    # Plot hydro data
+    # plotBEMIO(hydro)
+else
+    println("Invalid option selected. Please run the program again and select either 0 or 1.")
 end
-
-
